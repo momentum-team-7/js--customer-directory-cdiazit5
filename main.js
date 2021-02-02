59c1b1160bab926ed0a6b850dec71cd3c40e365c
cdiazit5 <cdiazit5@gmail.com>
2021-02-02 18:22:24 -0500

    newDiv method

--- main.js
+++ main.js
@@ -8,40 +8,40 @@ console.log("customer imgsrc is ", customer.picture.thumbnail)
 
 
 for (let customer of customers){
-    // const newDiv = document.createElement("div")
-    // newDiv.classname = 'profile'
+    const newDiv = document.createElement("div")
+    newDiv.classname = 'profile'
 
     const customerImg = document.createElement("img")
     customerImg.src= customer.picture.thumbnail
-    container.appendChild (customerImg)
+    newDiv.appendChild (customerImg)
 
     const customerName= document.createElement("h3")
     customerName.innerText= `name: ${customer.name.first} ${customer.name.last}`
     // issue not displaying the linked text ^
-    container.appendChild (customerName)
+    newDiv.appendChild (customerName)
 
     const customerEmail= document.createElement("h4")
     customerEmail.innerText = customer.email
-    container.appendChild (customerEmail)
+    newDiv.appendChild (customerEmail)
 
     const customerPhone= document.createElement("h4")
     customerPhone.innerText = customer.phone
-    container.appendChild(customerPhone)
+    newDiv.appendChild(customerPhone)
 
 
     const customerAddress= document.createElement("h4")
     customerAddress.innerText = `address: ${customer.location.street.number} ${customer.location.street.name} ${customer.location.city},  ${customer.location.postcode} ${nameToAbbr(customer.location.state)} ${customer.location.country}`
-    container.appendChild (customerAddress)
+    newDiv.appendChild (customerAddress)
 
     const customerBirthday= document.createElement("h4")
     customerBirthday.innerText = moment (customer.dob.date)
-    format ('ll');
+    // format ('ll')
     // displays [object Object] ^
-    container.appendChild (customerBirthday)
+    newDiv.appendChild (customerBirthday)
 
     const customerSince = document.createElement("h4")
     customerSince.innerText= `Customer since: `
-    container.appendChild (customerSince)
+    newDiv.appendChild (customerSince)
 
     // newDiv.appendChild (customerImg)
     // newDiv.appendChild (customerName)
@@ -54,7 +54,7 @@ for (let customer of customers){
 
 
 
-    // customers.appendChild (newDiv)
+    container.appendChild (newDiv)
     // ^ newDiv method
 }
 
